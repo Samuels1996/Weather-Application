3e2ef9a30206692f0bacf4580466fa0f8699218c
Brian Samuels <samuels.brian9@gmail.com>
2022-04-05 23:54:14 -0400

    adding in additonal js, index, as well as css files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Weather-Application
 This is a page for local weather, you can also change your city!
+
+
+This page was made to be able to look up any city and find weather for that area as well as extended out for the next 5 days.
+
--- assets/js/newScript.js
+++ assets/js/newScript.js
@@ -1,11 +1,13 @@
 var apiKey ='7adddd61a6c99ba40eb99e6a48cd59a1'
 var searchBtnEl = $("#searchBtn")
+var today = moment();
+$('.date').text(today.format('L'))
 
-function currentWeather (cords) {
-    var url1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apikey;
-    var city = $('.city-name').val();
+function currentWeather () {
+    // var url1 = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey;
+    var city = $('#city-name').val();
     
-    fetch(url1)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey)
         .then(function (response) {
             console.log(response);
             return response.json();
@@ -16,11 +18,32 @@ function currentWeather (cords) {
                 var cityName = data.name;
                 var humidity = data.main.humidity;
                 var windSpeed = data.wind.speed;
-                
+                var longitude = data.coord.lon;
+                var latitude = data.coord.lat;
+                var temperature = data.main.temp;
+                var icon = data.weather[0].icon;
+                var todayWeather = [cityName, humidity, windSpeed, longitude, latitude, temperature, icon]
 
+                console.log(todayWeather)
+                console.log(currentWeather)
 
+                document.querySelector('.cityNames').innerHTML = cityName;
+                document.querySelector('.humidity').innerHTML = humidity + '%';
+                document.querySelector('.nowTemp').innerHTML = temperature +'°F';
+                document.querySelector('.currentIcon').src = 'http://openweathermap.org/img/wn/' + icon + '@2x.png';
+                document.querySelector('.windSpeed').innerHTML = windSpeed +'mph';
+
+              
             })
 
-
-
     };
+
+//     $.ajax({
+//         url:url1,
+//         method:"GET",
+//     })
+// }
+// currentWeather()
+
+    var searchBtnEl = $('#searchBtn');
+    searchBtnEl.on('click', currentWeather)
